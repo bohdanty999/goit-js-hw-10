cd3b629150948f83708b84eaf5357b718beac696
bohdanty999 <bohdanty1@gmail.com>
2026-05-31 22:59:06 +0000

    Deploying to gh-pages from @ bohdanty999/goit-js-hw-10@8e76b0547dc1db7306b7fc8085689c613b372ff0 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t=document.querySelector("form"),o=document.querySelector('[name="delay"]');o.addEventListener("input",e=>{console.log()});t.addEventListener("submit",e=>{e.preventDefault});console.log(o);
+//# sourceMappingURL=2-snackbar.js.map
