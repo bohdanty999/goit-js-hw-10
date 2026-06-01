315a89d9d4c0fd7f3f3ecf2fea06574d9b6f87c1
bohdanty999 <bohdanty1@gmail.com>
2026-06-01 20:28:59 +0100

    task 2 finish

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,9 +1,33 @@
-const form = document.querySelector('form');
-const delay = document.querySelector('[name="delay"]');
-delay.addEventListener('input', e => {
-  console.log();
-});
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
 form.addEventListener('submit', e => {
-  e.preventDefault;
+  e.preventDefault();
+
+  const delay = form.elements.delay.value;
+  const state = form.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay =>
+      iziToast.show({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      })
+    )
+    .catch(delay =>
+      iziToast.show({
+        message: `❌ Rejected promise in ${delay}ms`,
+      })
+    );
 });
-console.log(delay);
